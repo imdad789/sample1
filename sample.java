b9116f648df8a41d2705bbc8c8086e5c27f2d8fd
imdad789 <95198946+imdad789@users.noreply.github.com>
2022-06-23 18:35:00 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -4,7 +4,7 @@ class Sample{
 	
 		string.out.println("Hello Welcome");
 		string.out.println("To JAVA");
-		string.out.println("Branch1 added-conflict-added");
+		string.out.println("Branch1 added-conflict-deleted");
 	}
 
 }
