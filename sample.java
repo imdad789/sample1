0df1cba49bbc8314ce67d8367d3b4a8a96836890
imdadu <s.imdad789@gmail.com>
2022-07-10 13:29:55 +0530

    1st com

--- sample.java
+++ sample.java
@@ -4,10 +4,6 @@ class Sample{
 	
 		string.out.println("Hello Welcome");
 		string.out.println("To JAVA");
-		string.out.println("Branch1 added-conflict-added-1");
-		string.out.println("Branching");
-		String.out.println("Branching1");
-		string.out.println("Branch1 added-conflict-deleted");
 
 	}
 
